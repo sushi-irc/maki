e70eff4f317522f9eb0645ecae28c519bb9e5960
Michael Kuhn <suraia@ikkoku.de>
2008-10-14 16:20:56 +0200

    Daemonize later.

--- source/maki.c
+++ source/maki.c
@@ -143,7 +143,15 @@ int main (int argc, char* argv[])
 
 	if (!maki_dbus_connected(dbus))
 	{
+		/* Use g_warning() here to not overwrite the debug log of the already running maki. */
 		g_warning("%s\n", _("Could not connect to DBus. maki may already be running."));
+		g_object_unref(dbus);
+		return 1;
+	}
+
+	if ((inst = maki_instance_get_default()) == NULL)
+	{
+		g_warning("%s\n", _("Could not create maki instance."));
 		g_object_unref(dbus);
 		return 1;
 	}
@@ -151,14 +159,8 @@ int main (int argc, char* argv[])
 	if (opt_daemon
 	    && maki_daemonize() != 0)
 	{
-		/* FIXME error message */
-		g_object_unref(dbus);
-		return 1;
-	}
-
-	if ((inst = maki_instance_get_default()) == NULL)
-	{
-		/* FIXME error message */
+		g_warning("%s\n", _("Could not switch to daemon mode."));
+		maki_instance_free(inst);
 		g_object_unref(dbus);
 		return 1;
 	}
