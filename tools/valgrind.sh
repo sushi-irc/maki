4d06c8e8d45e68f516c4fd5e4bff6706ec79ee3d
Michael Kuhn <suraia@ikkoku.de>
2009-05-31 19:50:01 +0200

    Add Valgrind tool.

---
+++ tools/valgrind.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+# Copyright (c) 2008-2009 Michael Kuhn
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHORS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+
+usage ()
+{
+	echo "Usage: ${0##*/} program [arguments]" >&2
+	exit 1
+}
+
+export G_SLICE=always-malloc
+export G_DEBUG=gc-friendly
+
+dirname="${0%/*}"
+program="${1##*/}"
+
+test -z "${dirname}" && dirname='.'
+test -z "${program}" && usage
+
+valgrind \
+	--tool=memcheck \
+	--leak-check=full \
+	--leak-resolution=high \
+	--trace-children=yes \
+	--num-callers=20 \
+	--suppressions="${dirname}/${program}.sup" \
+	--gen-suppressions=yes \
+	"$@"
